df2266cca4a209e636300225a79ece1fe033d670
annawa123 <69251160+annawa123@users.noreply.github.com>
2020-08-05 11:33:32 -0400

    First Xcode Project

---
+++ Intro-to-Xcode&Storyboard/Intro-to-Xcode&Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intro-to-Xcode&Storyboard
+//
+//  Created by Anna Waldbaum on 8/5/20.
+//  Copyright © 2020 Anna Waldbaum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement!")
+    }
+
+
+}
+
